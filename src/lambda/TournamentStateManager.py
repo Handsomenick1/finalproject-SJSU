4b26093b5393803cd80183c7c7651e552a516623
Fuyu Zhang <iamnickchang@hotmail.com>
2022-10-30 14:55:59 -0500

    init

--- src/lambda/TournamentStateManager.py
+++ src/lambda/TournamentStateManager.py
@@ -5,9 +5,6 @@ logger.setLevel(logging.DEBUG)
 def lambda_handler(event, context):
     """_summary_
 
-    1, track tournament state
-    2, manage the realted reources
-
     """
     return {
         "statusCode": 200
