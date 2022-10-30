ecda080562f90fbc6392811fbce715d0eddb33c1
Fuyu Zhang <iamnickchang@hotmail.com>
2022-10-30 14:45:11 -0500

    add two handlers

--- src/lambda/GroupManager.py
+++ src/lambda/GroupManager.py
@@ -2,14 +2,27 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-def lambda_handler(event, context):
+def startGroups_handler(event, context):
     """_summary_
     
     1, Assign groups in QUEUED into rooms (if no room is available, wait until the next call)
     2, Collects judging results
     3, Passes control back to the roundManager if all groups are completed.
 
+    """
     
+    
+    return {
+        "statusCode": 200
+    }
+
+def collectResult_handler(event, context):
+    """_summary_
+    
+    1, Assign groups in QUEUED into rooms (if no room is available, wait until the next call)
+    2, Collects judging results
+    3, Passes control back to the roundManager if all groups are completed.
+
     """
     
     
