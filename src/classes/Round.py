9e39cbb2b254fa9f66b8593461668e9cda21d5d5
Fuyu Zhang <iamnickchang@hotmail.com>
2022-11-01 17:58:58 -0500

    add methods

--- src/classes/Round.py
+++ src/classes/Round.py
@@ -20,6 +20,9 @@ class Round:
     def get_STARTED(self):
         return self.STARTED
     
+    def get_COMPLETED(self):
+        return self.COMPLETED
+    
     def QUEUED_to_ASSIGNED(self, groups):
         for group in groups:
             if group in self.QUEUED:
@@ -34,3 +37,17 @@ class Round:
         for group in groups:
             if group in self.STARTED:
                 self.COMPLETED.append(group) 
+    
+    def get_round_info(self):
+        return {
+            "roundId" : self.get_id(),
+            "QUEUED" : self.get_QUEUED(),
+            "ASSIGNED" : self.get_ASSIGNED(),
+            "STARTED" : self.get_STARTED(),
+            "COMPELETED": self.get_COMPLETED()
+        }  
+    
+    def get_round_json(self):
+        return json.dumps(
+            self.get_round_info()
+        )  
